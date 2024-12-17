2ce43f09060f118d7ba9277766ffdc42bb9f7a9b
andikaPalian <andika12des@gmail.com>
2024-12-17 14:03:00 +0800

    mengupdate user dengan membuat login backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -29,4 +29,29 @@ const registerUser = async (req, res) => {
     };
 };
 
-module.exports = {registerUser};
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if (!email || !password) {
+            return res.status(400).json({message: "Please fill all the fields"});
+        };
+        const user = await User.findOne({email});
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign({
+                id: user._id,
+            }, process.env.ACCESS_TOKEN, {expiresIn: "1d"});
+            user.password = undefined;
+            res.status(200).json({message: "User login successfully", data: {user, token}});
+        } else {
+            return res.status(400).json({message:"Invalid Email or Password"});
+        };
+    } catch (error) {
+        console.error("Error in loginUser controller:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "Am unexpected error occurred",
+        });
+    };
+};
+
+module.exports = {registerUser, loginUser};
